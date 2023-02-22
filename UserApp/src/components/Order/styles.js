a4612f441967618edc048c1675f54d416ee54c5a
Jagriti <jagritim2002@gmail.com>
2023-02-22 21:04:10 +0530

    first commit

---
+++ UserApp/src/components/Order/styles.js
@@ -0,0 +1,29 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+    container:{
+        backgroundColor:'#fff',
+    },
+    image:{
+        height:200,
+        width:250,
+        resizeMode:'contain',
+        marginLeft:60,
+        top:10,
+    },
+    text1:{
+        color:'#006400',
+        fontSize:30,
+        fontWeight:'bold',
+        textAlign:'center',
+    },
+    text2:{
+        color:'#000',
+        fontSize:30,
+        fontWeight:'bold',
+        textAlign:'center',
+        top:30,
+    },
+});
+
+export default styles;
